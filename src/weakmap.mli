6f76b07047dc6e0d20c4074077f9ce74c8bace84
Thibaut <tgudin@besport.com>
2021-08-31 18:03:11 +0200

    adding 'set' and 'get' functions into the 'Weakmap' library

--- src/weakmap.mli
+++ src/weakmap.mli
@@ -1,1 +1,20 @@
-(*TODO*)
+[@@@js.stop]
+
+val available : unit -> bool
+
+[@@@js.start]
+
+[@@@js.implem
+let available () =
+  Js_of_ocaml.Js.Optdef.test Js_of_ocaml.Js.Unsafe.global ##. WeakMap]
+
+type weakmap
+
+type key = Ojs.t
+
+val new_weakmap : ?iterable:(key * Ojs.t) array -> unit -> weakmap
+  [@@js.new "WeakMap"]
+
+val get : weakmap -> key -> Ojs.t [@@js.call]
+
+val set : weakmap -> key -> Ojs.t -> unit [@@js.call]
